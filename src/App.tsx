a966762bdbbacba43ea92d73b1e7e3e0c29658a7
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-11-21 20:37:34 +0530

    Adding custom fonts, tailwind support and app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+// css & icons
+import "./App.css";
+// components
+import Header from "./components/Header";
+import Section01 from "./components/Section01";
+import Section02 from "./components/Section02";
+import Section03 from "./components/Section03";
+import Section04 from "./components/Section04";
+import Section05 from "./components/Section05";
+import Section06 from "./components/Section06";
+import Footer from "./components/Footer";
+
 function App() {
-  return <h1 className="mt-5 text-xl text-center">StableTrust</h1>;
+  return (
+    <div className="xl:relative mx-auto max-w-screen-2xl w-full!">
+      <Header />
+      <Section01 />
+      <Section02 />
+      <Section03 />
+      <Section04 />
+      <Section05 />
+      <Section06 />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
